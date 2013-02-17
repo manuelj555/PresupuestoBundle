b780b618d0a0e48adbfffd070c3021e2d0ae7380
Manuel Aguirre <programador.manuel@gmail.com>
2013-02-16 21:27:42 -0430

    se agrega hace uso del bundle KnpPaginatorBundle...
    y se paginan los presupuestos

--- Controller/PresupuestoController.php
+++ Controller/PresupuestoController.php
@@ -9,9 +9,11 @@ class PresupuestoController extends Controller
 
     public function listadoAction()
     {
-        $presupuestos = $this->getDoctrine()->getManager()
-                ->getRepository("PresupuestoBundle:Presupuestos")
-                ->findAll();
+        $query = $this->getDoctrine()->getEntityManager()
+                ->createQuery("SELECT p FROM PresupuestoBundle:Presupuestos p");
+
+        $presupuestos = $this->get("knp_paginator")->paginate($query
+                , $this->getRequest()->query->get("page", 1));
 
         return $this->render("PresupuestoBundle:Presupuesto:listado.html.twig", array(
                     'presupuestos' => $presupuestos,
