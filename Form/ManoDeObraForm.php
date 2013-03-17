08c0db13242d2de3559ae5eef2e414cf488e066b
manuel_j555 <programador.manuel@gmail.com>
2013-03-17 19:17:29 -0430

    agregando el plugin validation engine..

--- Form/ManoDeObraForm.php
+++ Form/ManoDeObraForm.php
@@ -5,41 +5,49 @@ namespace K2\PresupuestoBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class ManoDeObraForm extends AbstractType
-{
+class ManoDeObraForm extends AbstractType {
 
-    public function getName()
-    {
+    public function getName() {
         return "mano_de_obra";
     }
 
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder->add('descripcion', null, array(
                     'label' => "Descripción",
                     'error_bubbling' => true,
+                    'attr' => array(
+                        'class' => "validate[required]",
+                    ),
                 ))
                 ->add('medidas', 'entity', array(
                     'label' => "Unidad de Medida",
                     'class' => "K2\\PresupuestoBundle\\Entity\\Medidas",
                     'property' => 'medida',
                     'error_bubbling' => true,
+                    'attr' => array(
+                        'class' => "validate[required]",
+                    ),
                 ))
                 ->add('tiposDeObras', 'entity', array(
                     'label' => "Tipo",
                     'class' => "K2\\PresupuestoBundle\\Entity\\TiposDeObras",
                     'property' => 'nombre',
                     'error_bubbling' => true,
+                    'attr' => array(
+                        'class' => "validate[required]",
+                    ),
                 ))
                 ->add('precio', 'text', array(
                     'label' => "Precio",
                     'error_bubbling' => true,
+                    'attr' => array(
+                        'class' => "validate[required]",
+                    ),
                 ))
         ;
     }
 
-    public function setDefaultOptions(\Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(\Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver) {
         parent::setDefaultOptions($resolver);
         $resolver->setDefaults(array(
             'csrf_protection' => false,
